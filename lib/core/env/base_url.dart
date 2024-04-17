d1f00164b6bd4451dc404b01401401cc8f1d75d2
Noman5253 <nomankhanbhai@gmail.com>
2024-04-17 23:36:26 +0530

    removed unwanted files

--- lib/core/env/base_url.dart
+++ lib/core/env/base_url.dart
@@ -3,12 +3,3 @@ abstract class BaseUrl {
 
   String getBaseUrl();
 }
-
-class WeatherApiBaseUrl extends BaseUrl {
-  const WeatherApiBaseUrl();
-  
-  @override
-  String getBaseUrl() {
-    return "https://api.tomorrow.io/v4/weather/";
-  }
-}
--- lib/core/env/weather_api_key.dart
+++
@@ -1,3 +0,0 @@
-class WeatherApiKey {
-  static const apiKey = "pR6EHEzvV9Fgao7V6lgvLWkOLMGWfucF";
-}
